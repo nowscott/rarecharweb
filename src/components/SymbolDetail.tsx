68afcc5553a0ba797749cfb7e9a3146eb56ea6ce
NowScott <nowscott@qq.com>
2025-06-27 10:49:56 +0800

    refactor(SymbolDetail): 移除多余的滚动指示器图标

--- src/components/SymbolDetail.tsx
+++ src/components/SymbolDetail.tsx
@@ -166,14 +166,7 @@ const SymbolDetail: React.FC<SymbolDetailProps> = ({ symbol, onClose }) => {
                   {showScrollGradient && (
                     <div className="absolute bottom-0 left-0 right-2 h-6 bg-gradient-to-t from-gray-100 via-gray-100/80 to-transparent dark:from-gray-700/50 dark:via-gray-700/70 dark:to-transparent pointer-events-none" />
                   )}
-                  {/* 滚动指示器 - 仅在内容溢出时显示 */}
-                  {showScrollGradient && (
-                    <div className="absolute top-1 right-1 text-gray-400 dark:text-gray-500 text-xs opacity-60">
-                      <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-                  )}
+
                 </div>
               </div>
             )}
